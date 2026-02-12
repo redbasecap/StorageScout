0ce7bfa31e8555449a48132bef105da08532b2a3
Nicola Spieser <spieser.nicola@gmail.com>
2026-02-12 22:59:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-956002117-4adf6",
+  "appId": "1:666134697250:web:7bfb2d6ca00f82f0e3017b",
+  "apiKey": "AIzaSyDTm5-NQm0DS6YGzEEY5GToi4DYz33eDqk",
+  "authDomain": "studio-956002117-4adf6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "666134697250"
+};
